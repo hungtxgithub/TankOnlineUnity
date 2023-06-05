1c58c50d7e295b74ca3dca33eae69aa9338f9b7c
thiendb <73954171+John-0404@users.noreply.github.com>
2023-06-05 23:51:26 +0700

    done loadmap

--- TankOnlineFU/Assets/Scripts/LoadFile.cs
+++ TankOnlineFU/Assets/Scripts/LoadFile.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class LoadFile : MonoBehaviour
 {
     private static LoadFile instance;
@@ -28,14 +28,49 @@ public class LoadFile : MonoBehaviour
 
     [SerializeField]
     public GameObject gameObjectWater;
-    private void Awake()
-    {
-        instance = this;
-    }
+
     // Start is called before the first frame update
-    public void LoadConstructionMap()
+    public void LoadMap()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-       // SaveFile.Instance.
+        List<MapData> datas = SaveFile.Instance.loadFile();
+        foreach (MapData data in datas)
+        {
+            switch (data.objectType)
+            {
+                case "Brick1":
+                    Instantiate(gameObjectBrick[0],new Vector2(data.positionX, data.positionY), Quaternion.identity);
+                    break;
+                case "Brick2":
+                    Instantiate(gameObjectBrick[1],new Vector2(data.positionX, data.positionY), Quaternion.identity);
+                    break;
+                case "Brick3":
+                    Instantiate(gameObjectBrick[2],new Vector2(data.positionX, data.positionY), Quaternion.identity);
+                    break;
+                case "BrickCell":
+                    Instantiate(gameObjectBrick[3],new Vector2(data.positionX, data.positionY), Quaternion.identity);
+                    break;
+                case "Stone1":
+                    Instantiate(gameObjectStone[0],new Vector2(data.positionX, data.positionY), Quaternion.identity);
+                    break;
+                case "Stone2":
+                    Instantiate(gameObjectStone[1],new Vector2(data.positionX, data.positionY), Quaternion.identity);
+                    break;
+                case "Stone3":
+                    Instantiate(gameObjectStone[2],new Vector2(data.positionX, data.positionY), Quaternion.identity);
+                    break;
+                case "StoneCell":
+                    Instantiate(gameObjectStone[3],new Vector2(data.positionX, data.positionY), Quaternion.identity);
+                    break;
+                case "Trees":
+                    Instantiate(gameObjectStrees,new Vector2(data.positionX, data.positionY), Quaternion.identity);
+                    break;
+                case "Water":
+                    Instantiate(gameObjectWater,new Vector2(data.positionX, data.positionY), Quaternion.identity);
+                    break;
+            }
+
+
+        }
     }
 }
--- TankOnlineFU/Assets/Scripts/Menu.cs
+++ TankOnlineFU/Assets/Scripts/Menu.cs
@@ -5,8 +5,60 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+     [SerializeField]
+    public List<GameObject> gameObjectBrick;
+
+    [SerializeField]
+    public List<GameObject> gameObjectStone;
+
+    [SerializeField]
+    public GameObject gameObjectStrees;
+
+    [SerializeField]
+    public GameObject gameObjectWater;
     public void LoadConstructionMap()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void LoadMap()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        List<MapData> datas = SaveFile.Instance.loadFile();
+        foreach (MapData data in datas)
+        {
+            switch (data.objectType)
+            {
+                case "Brick1":
+                    Instantiate(gameObjectBrick[0], new Vector2(data.positionX, data.positionY), Quaternion.identity);
+                    break;
+                case "Brick2":
+                    Instantiate(gameObjectBrick[1], new Vector2(data.positionX, data.positionY), Quaternion.identity);
+                    break;
+                case "Brick3":
+                    Instantiate(gameObjectBrick[2], new Vector2(data.positionX, data.positionY), Quaternion.identity);
+                    break;
+                case "BrickCell":
+                    Instantiate(gameObjectBrick[3], new Vector2(data.positionX, data.positionY), Quaternion.identity);
+                    break;
+                case "Stone1":
+                    Instantiate(gameObjectStone[0], new Vector2(data.positionX, data.positionY), Quaternion.identity);
+                    break;
+                case "Stone2":
+                    Instantiate(gameObjectStone[1], new Vector2(data.positionX, data.positionY), Quaternion.identity);
+                    break;
+                case "Stone3":
+                    Instantiate(gameObjectStone[2], new Vector2(data.positionX, data.positionY), Quaternion.identity);
+                    break;
+                case "StoneCell":
+                    Instantiate(gameObjectStone[3], new Vector2(data.positionX, data.positionY), Quaternion.identity);
+                    break;
+                case "Trees":
+                    Instantiate(gameObjectStrees, new Vector2(data.positionX, data.positionY), Quaternion.identity);
+                    break;
+                case "Water":
+                    Instantiate(gameObjectWater, new Vector2(data.positionX, data.positionY), Quaternion.identity);
+                    break;
+            }
+        }
     }
 }
